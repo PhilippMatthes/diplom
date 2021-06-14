b8bfc5b46a43fc4c547d6996763284257cd377ce
PhilippMatthes <github@philippmatth.es>
2021-06-14 13:21:29 +0200

    Update thesis

--- src/model-size-calculation.py
+++ src/model-size-calculation.py
@@ -15,36 +15,38 @@ matusek_model.add(Dense(256, activation="relu"))
 matusek_model.add(Dense(7, activation="softmax"))
 matusek_model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-plot_model(matusek_model, to_file="matusek-model.pdf", show_shapes=True, show_layer_names=False)
+# plot_model(matusek_model, to_file="matusek-model.pdf", show_shapes=True, show_layer_names=False)
 
 with tempfile.TemporaryDirectory() as tmp_dir:
     filename = f"{tmp_dir}/model.h5"
     matusek_model.save(filename)
     print(f"Matusek model size in Bytes: {os.path.getsize(filename)}")
+    trainable_count = int(np.sum([K.count_params(p) for p in set(matusek_model.trainable_weights)]))
+    print(f"Matusek model params: {trainable_count}")
 
 werner_model_left = Sequential()
 werner_model_left.add(LSTM(64, input_shape=(64,1), return_sequences=True))
 werner_model_left.add(LSTM(64, return_sequences=True))
 werner_model_left.add(Dense(64, activation="relu"))
-# werner_model_left.add(LeakyReLU(alpha=0.05))
+werner_model_left.add(LeakyReLU(alpha=0.05))
 werner_model_left.add(Dense(32, activation="relu"))
-# werner_model_left.add(LeakyReLU(alpha=0.05))
+werner_model_left.add(LeakyReLU(alpha=0.05))
 
 werner_model_center = Sequential()
 werner_model_center.add(Dense(64, input_shape=(64,1)))
-# werner_model_center.add(LeakyReLU(alpha=0.05))
+werner_model_center.add(LeakyReLU(alpha=0.05))
 werner_model_center.add(Dense(48, activation="relu"))
-# werner_model_center.add(LeakyReLU(alpha=0.05))
+werner_model_center.add(LeakyReLU(alpha=0.05))
 werner_model_center.add(Dense(32, activation="relu"))
-# werner_model_center.add(LeakyReLU(alpha=0.05))
+werner_model_center.add(LeakyReLU(alpha=0.05))
 
 werner_model_right = Sequential()
 werner_model_right.add(LSTM(64, input_shape=(64,1), return_sequences=True))
 werner_model_right.add(LSTM(64, return_sequences=True))
 werner_model_right.add(Dense(64, activation="relu"))
-# werner_model_right.add(LeakyReLU(alpha=0.05))
+werner_model_right.add(LeakyReLU(alpha=0.05))
 werner_model_right.add(Dense(32, activation="relu"))
-# werner_model_right.add(LeakyReLU(alpha=0.05))
+werner_model_right.add(LeakyReLU(alpha=0.05))
 
 werner_model_conc = concatenate([
     werner_model_left.output,
@@ -52,9 +54,9 @@ werner_model_conc = concatenate([
     werner_model_right.output
 ], axis=-1)
 werner_model_conc = Dense(128, activation="relu")(werner_model_conc)
-# werner_model_conc = LeakyReLU(alpha=0.05)(werner_model_conc)
+werner_model_conc = LeakyReLU(alpha=0.05)(werner_model_conc)
 werner_model_conc = Dense(64, activation="relu")(werner_model_conc)
-# werner_model_conc = LeakyReLU(alpha=0.05)(werner_model_conc)
+werner_model_conc = LeakyReLU(alpha=0.05)(werner_model_conc)
 werner_model_conc = Dense(7, activation="softmax")(werner_model_conc)
 
 werner_model = Model(inputs=[
@@ -69,8 +71,10 @@ with tempfile.TemporaryDirectory() as tmp_dir:
     filename = f"{tmp_dir}/model.h5"
     werner_model.save(filename)
     print(f"Werner model size in Bytes: {os.path.getsize(filename)}")
+    trainable_count = int(np.sum([K.count_params(p) for p in set(werner_model.trainable_weights)]))
+    print(f"Werner model params: {trainable_count}")
 
-plot_model(werner_model, to_file="werner-model.pdf",show_shapes=True, show_layer_names=False)
+# plot_model(werner_model, to_file="werner-model.pdf",show_shapes=True, show_layer_names=False)
 
 
 # Unklarheiten: Ort des Dropout, Ort der L2-Regularisierung, Input-Shape
@@ -93,6 +97,7 @@ with tempfile.TemporaryDirectory() as tmp_dir:
     filename = f"{tmp_dir}/model.h5"
     stojanov_model.save(filename)
     print(f"Stojanov model size in Bytes: {os.path.getsize(filename)}")
+    trainable_count = int(np.sum([K.count_params(p) for p in set(stojanov_model.trainable_weights)]))
+    print(f"Stojanov model summary: {trainable_count}")
 
-
-plot_model(stojanov_model, to_file="stojanov-model.pdf",show_shapes=True, show_layer_names=False)
+# plot_model(stojanov_model, to_file="stojanov-model.pdf",show_shapes=True, show_layer_names=False)
