aebffdf8f2c292e294d47f03edec406ca64a2ca8
PhilippMatthes <github@philippmatth.es>
2021-08-06 09:16:01 +0200

    Convert nans to zeros in dataset loader [skip ci]

--- src/tools/dataset.py
+++ src/tools/dataset.py
@@ -49,34 +49,34 @@ class SHLDataset:
 
 
 def load_shl_dataset(dataset_dir: pathlib.Path):
-    acc_x = pd.read_csv(dataset_dir / 'Acc_x.txt', header=None, sep=' ').to_numpy()
+    acc_x = np.nan_to_num(pd.read_csv(dataset_dir / 'Acc_x.txt', header=None, sep=' ').to_numpy())
     print('Acc_x Import Done')
-    acc_y = pd.read_csv(dataset_dir / 'Acc_y.txt', header=None, sep=' ').to_numpy()
+    acc_y = np.nan_to_num(pd.read_csv(dataset_dir / 'Acc_y.txt', header=None, sep=' ').to_numpy())
     print('Acc_y Import Done')
-    acc_z = pd.read_csv(dataset_dir / 'Acc_z.txt', header=None, sep=' ').to_numpy()
+    acc_z = np.nan_to_num(pd.read_csv(dataset_dir / 'Acc_z.txt', header=None, sep=' ').to_numpy())
     print('Acc_z Import Done')
     acc_mag = np.sqrt(acc_x**2 + acc_y**2 + acc_z**2)
     print('Acc_mag Import Done')
 
-    mag_x = pd.read_csv(dataset_dir / 'Mag_x.txt', header=None, sep=' ').to_numpy()
+    mag_x = np.nan_to_num(pd.read_csv(dataset_dir / 'Mag_x.txt', header=None, sep=' ').to_numpy())
     print('Mag_x Import Done')
-    mag_y = pd.read_csv(dataset_dir / 'Mag_y.txt', header=None, sep=' ').to_numpy()
+    mag_y = np.nan_to_num(pd.read_csv(dataset_dir / 'Mag_y.txt', header=None, sep=' ').to_numpy())
     print('Mag_y Import Done')
-    mag_z = pd.read_csv(dataset_dir / 'Mag_z.txt', header=None, sep=' ').to_numpy()
+    mag_z = np.nan_to_num(pd.read_csv(dataset_dir / 'Mag_z.txt', header=None, sep=' ').to_numpy())
     print('Mag_z Import Done')
     mag_mag = np.sqrt(mag_x**2 + mag_y**2 + mag_z**2)
     print('Mag_mag Import Done')
 
-    gyr_x = pd.read_csv(dataset_dir / 'Gyr_x.txt', header=None, sep=' ').to_numpy()
+    gyr_x = np.nan_to_num(pd.read_csv(dataset_dir / 'Gyr_x.txt', header=None, sep=' ').to_numpy())
     print('Gyr_x Import Done')
-    gyr_y = pd.read_csv(dataset_dir / 'Gyr_y.txt', header=None, sep=' ').to_numpy()
+    gyr_y = np.nan_to_num(pd.read_csv(dataset_dir / 'Gyr_y.txt', header=None, sep=' ').to_numpy())
     print('Gyr_y Import Done')
-    gyr_z = pd.read_csv(dataset_dir / 'Gyr_z.txt', header=None, sep=' ').to_numpy()
+    gyr_z = np.nan_to_num(pd.read_csv(dataset_dir / 'Gyr_z.txt', header=None, sep=' ').to_numpy())
     print('Gyr_z Import Done')
     gyr_mag = np.sqrt(gyr_x**2 + gyr_y**2 + gyr_z**2)
     print('Gyr_mag Import Done')
 
-    labels = pd.read_csv(dataset_dir / 'Label.txt', header=None, sep=' ').mode(axis=1).to_numpy().flatten()
+    labels = np.nan_to_num(pd.read_csv(dataset_dir / 'Label.txt', header=None, sep=' ').mode(axis=1).to_numpy().flatten())
     print('Labels Import Done')
 
     return SHLDataset(
