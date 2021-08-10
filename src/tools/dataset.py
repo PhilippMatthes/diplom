ebbe62fb13de2590653af610939b6299c5ed1858
PhilippMatthes <github@philippmatth.es>
2021-08-10 13:09:14 +0200

    Refactor dataset loading to include all dataset sensor tracks [skip ci]

--- src/tools/dataset.py
+++ src/tools/dataset.py
@@ -18,94 +18,54 @@ shl_dataset_label_order = [
     'Subway',
 ]
 
+shl_dataset_X_attributes = [
+    'acc_x', 'acc_y', 'acc_z',
+    'mag_x', 'mag_y', 'mag_z',
+    'gyr_x', 'gyr_y', 'gyr_z',
+    'gra_x', 'gra_y', 'gra_z',
+    'lacc_x', 'lacc_y', 'lacc_z',
+    'ori_x', 'ori_y', 'ori_z', 'ori_w',
+]
+
+shl_dataset_y_attributes = ['labels']
+
+shl_dataset_attributes = shl_dataset_X_attributes + shl_dataset_y_attributes
+
+shl_dataset_files = [
+    'Acc_x.txt', 'Acc_y.txt', 'Acc_z.txt',
+    'Mag_x.txt', 'Mag_y.txt', 'Mag_z.txt',
+    'Gyr_x.txt', 'Gyr_y.txt', 'Gyr_z.txt',
+    'Gra_x.txt', 'Gra_y.txt', 'Gra_z.txt',
+    'LAcc_x.txt', 'LAcc_y.txt', 'LAcc_z.txt',
+    'Ori_x.txt', 'Ori_y.txt', 'Ori_z.txt', 'Ori_w.txt',
+    'Label.txt'
+]
+
 
 class SHLDataset:
-    def __init__(
-        self,
-        acc_x, acc_y, acc_z,
-        acc_mag,
-        mag_x, mag_y, mag_z,
-        mag_mag,
-        gyr_x, gyr_y, gyr_z,
-        gyr_mag,
-        labels
-    ):
-        self.acc_x = acc_x
-        self.acc_y = acc_y
-        self.acc_z = acc_z
-        self.acc_mag = acc_mag
-
-        self.mag_x = mag_x
-        self.mag_y = mag_y
-        self.mag_z = mag_z
-        self.mag_mag = mag_mag
-
-        self.gyr_x = gyr_x
-        self.gyr_y = gyr_y
-        self.gyr_z = gyr_z
-        self.gyr_mag = gyr_mag
-        
-        self.labels = labels
+    def __init__(self):
+        pass
 
     def concat_inplace(self, other):
-        self.acc_x = np.concatenate((self.acc_x, other.acc_x), axis=0)
-        self.acc_y = np.concatenate((self.acc_y, other.acc_y), axis=0)
-        self.acc_z = np.concatenate((self.acc_z, other.acc_z), axis=0)
-        self.acc_mag = np.concatenate((self.acc_mag, other.acc_mag), axis=0)
-
-        self.mag_x = np.concatenate((self.mag_x, other.mag_x), axis=0)
-        self.mag_y = np.concatenate((self.mag_y, other.mag_y), axis=0)
-        self.mag_z = np.concatenate((self.mag_z, other.mag_z), axis=0)
-        self.mag_mag = np.concatenate((self.mag_mag, other.mag_mag), axis=0)
-
-        self.gyr_x = np.concatenate((self.gyr_x, other.gyr_x), axis=0)
-        self.gyr_y = np.concatenate((self.gyr_y, other.gyr_y), axis=0)
-        self.gyr_z = np.concatenate((self.gyr_z, other.gyr_z), axis=0)
-        self.gyr_mag = np.concatenate((self.gyr_mag, other.gyr_mag), axis=0)
-
-        self.labels = np.concatenate((self.labels, other.labels), axis=0)
+        for attribute in shl_dataset_attributes:
+            setattr(self, attribute, np.concatenate((
+                getattr(self, attribute),
+                getattr(other, attribute)
+            ), axis=0))
 
 
-def load_shl_dataset(dataset_dir: pathlib.Path, nrows=None):
-    acc_x = np.nan_to_num(pd.read_csv(dataset_dir / 'Acc_x.txt', header=None, sep=' ', nrows=nrows).to_numpy())
-    print('Acc_x Import Done')
-    acc_y = np.nan_to_num(pd.read_csv(dataset_dir / 'Acc_y.txt', header=None, sep=' ', nrows=nrows).to_numpy())
-    print('Acc_y Import Done')
-    acc_z = np.nan_to_num(pd.read_csv(dataset_dir / 'Acc_z.txt', header=None, sep=' ', nrows=nrows).to_numpy())
-    print('Acc_z Import Done')
-    acc_mag = np.sqrt(acc_x**2 + acc_y**2 + acc_z**2)
-    print('Acc_mag Import Done')
-
-    mag_x = np.nan_to_num(pd.read_csv(dataset_dir / 'Mag_x.txt', header=None, sep=' ', nrows=nrows).to_numpy())
-    print('Mag_x Import Done')
-    mag_y = np.nan_to_num(pd.read_csv(dataset_dir / 'Mag_y.txt', header=None, sep=' ', nrows=nrows).to_numpy())
-    print('Mag_y Import Done')
-    mag_z = np.nan_to_num(pd.read_csv(dataset_dir / 'Mag_z.txt', header=None, sep=' ', nrows=nrows).to_numpy())
-    print('Mag_z Import Done')
-    mag_mag = np.sqrt(mag_x**2 + mag_y**2 + mag_z**2)
-    print('Mag_mag Import Done')
-
-    gyr_x = np.nan_to_num(pd.read_csv(dataset_dir / 'Gyr_x.txt', header=None, sep=' ', nrows=nrows).to_numpy())
-    print('Gyr_x Import Done')
-    gyr_y = np.nan_to_num(pd.read_csv(dataset_dir / 'Gyr_y.txt', header=None, sep=' ', nrows=nrows).to_numpy())
-    print('Gyr_y Import Done')
-    gyr_z = np.nan_to_num(pd.read_csv(dataset_dir / 'Gyr_z.txt', header=None, sep=' ', nrows=nrows).to_numpy())
-    print('Gyr_z Import Done')
-    gyr_mag = np.sqrt(gyr_x**2 + gyr_y**2 + gyr_z**2)
-    print('Gyr_mag Import Done')
-
-    labels = np.nan_to_num(pd.read_csv(dataset_dir / 'Label.txt', header=None, sep=' ', nrows=nrows).mode(axis=1).to_numpy().flatten())
-    print('Labels Import Done')
-
-    return SHLDataset(
-        acc_x, acc_y, acc_z,
-        acc_mag,
-        mag_x, mag_y, mag_z, 
-        mag_mag,
-        gyr_x, gyr_y, gyr_z, 
-        gyr_mag,
-        labels
-    )
+def load_shl_dataset(dataset_dir: pathlib.Path, tqdm=None, nrows=None):
+    dataset = SHLDataset()
+    if tqdm is None:
+        tqdm = lambda x, desc: x # passthrough
+    for attribute, filename in tqdm(
+        list(zip(shl_dataset_attributes, shl_dataset_files)),
+        desc=f'Loading dataset subfiles'
+    ):
+        df = pd.read_csv(dataset_dir / filename, header=None, sep=' ', nrows=nrows, dtype=np.float32)
+        np_arr = np.nan_to_num(df.to_numpy())
+        setattr(dataset, attribute, np_arr)
+    return dataset
 
 
 def load_zipped_shl_dataset(zip_dir: pathlib.Path, tqdm=None, nrows=None, subdir_in_zip='train'):
@@ -122,7 +82,7 @@ def load_zipped_shl_dataset(zip_dir: pathlib.Path, tqdm=None, nrows=None, subdir
 
         result_dataset = None
         for sub_dir in sub_dirs:
-            sub_dataset = load_shl_dataset(train_dir / sub_dir, nrows=nrows)
+            sub_dataset = load_shl_dataset(train_dir / sub_dir, tqdm=tqdm, nrows=nrows)
             if result_dataset is None:
                 result_dataset = sub_dataset
             else:
--- src/tools/plots.py
+++ src/tools/plots.py
@@ -1,19 +1,7 @@
 import matplotlib.pyplot as plt
 
-from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
-
 
 # Convenience function to make boxplots black and white
 def decolorize_boxplot(boxplot):
     for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
         plt.setp(boxplot[element], color='black')
-
-
-def plot_confusion_matrix(y_pred, y_true, labels, display_labels):
-    cm = confusion_matrix(y_true, y_pred, labels=labels)
-
-    disp = ConfusionMatrixDisplay(
-        confusion_matrix=cm,
-        display_labels=display_labels
-    )
-    disp.plot(cmap=plt.cm.binary, colorbar=False, xticks_rotation=45)
