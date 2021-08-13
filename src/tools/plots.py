cb42671fa0eeadb133b0b5f0999785972294d235
PhilippMatthes <github@philippmatth.es>
2021-08-13 10:43:54 +0200

    Cleanup folders and make jupyter notebooks host their own aux code [skip ci]

--- src/tools/plots.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-
-# Convenience function to make boxplots black and white
-def decolorize_boxplot(boxplot):
-    for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
-        plt.setp(boxplot[element], color='black')
--- src/tools/slugify.py
+++
@@ -1,6 +0,0 @@
-import re
-
-
-def slugify(s):
-    s = str(s).strip().replace(' ', '_').lower()
-    return re.sub(r'(?u)[^-\w.]', '', s)
